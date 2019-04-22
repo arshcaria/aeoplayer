b8a76e6737da78fd2630775f346c3c43bbefc321
Jiaqi Liu <jiaqi.liu@hotmail.com>
2019-04-22 14:51:53 +0800

    Add FFmpeg 4.0.2 headers and static libs to the project.

--- app/src/main/jni/native-lib.cpp
+++ app/src/main/jni/native-lib.cpp
@@ -3,8 +3,12 @@
 //
 #include <jni.h>
 
+extern "C" {
+#include <libavcodec/avcodec.h>
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_aeoplayer_MainActivity_helloFromCpp(JNIEnv *env, jobject instance) {
-    const char *msg = "Hello From JNI";
+    const char *msg = av_version_info();
     return env->NewStringUTF(msg);
 }
